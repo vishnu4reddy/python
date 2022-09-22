4e22fd10d367c21b58a789cc6d9918128538eca0
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2022-09-22 17:09:16 +0530

    Add files via upload

---
+++ arrayall.py
@@ -0,0 +1,33 @@
+cars = ["benz","bmw","volovo"]
+x = cars[0]
+print(x)
+
+#
+cars = ["benz","bmw","volovo"]
+cars[0] = "toyota"
+print(x)
+
+# len
+cars = ["benz","bmw","volovo"]
+x = len(cars)
+print(x)
+
+# looping
+cars = ["benz","bmw","volovo"]
+for x in cars:
+    print(x)
+
+#adding
+cars = ["benz","bmw","volovo"]
+cars.append("honda")
+print(cars)
+
+#pop
+cars = ["benz","bmw","volovo"]
+cars.pop(0)
+print(cars)
+
+#remove    
+cars = ["benz","bmw","volovo"]
+cars.remove("bmw")
+print(cars)
