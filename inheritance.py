7d50349c1923775ce09b60b122f216504fdd7a64
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2022-09-23 18:10:53 +0530

    iterator

--- inheritance.py
+++ inheritance.py
@@ -58,3 +58,33 @@ b1 =B()
 c1 =C()
 a1.feature1()
 b1.feature4()
+
+# method resolution order
+class A :
+    def _init_(self):
+        print("in a init")
+    def feature1 (self):
+        print("feature 1 is working")
+    def feature2 (self):
+        print ("feature 2 is working")
+class B :
+    def _init_(self):
+        super()._init_()
+        print("in a init")
+    def feature3(self):
+        print("feature 3 is working")
+    def feature4(self):
+        print("feature 4 is working")
+class C(A,B) :
+    def feature5(self):
+        print("feature 5 is working")
+    def feature6(self):
+        print("feature 6 is working")
+    def feat(self):
+        super().feature2()
+a1 =A()
+b1 =B()
+c1 =C()
+a1.feature1()
+b1.feature4()
+
