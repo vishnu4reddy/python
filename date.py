1e957d0833e91a1913b2b89d73f114635d12aae6
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2022-09-27 16:42:01 +0530

    date

---
+++ date.py
@@ -0,0 +1,26 @@
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+#Date output
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print (x.strftime("%A"))
+
+# Date Objects
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+#Strftime method
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
