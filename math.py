14abd8e89ae2e1906b729e724962261ed2c2ba92
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2022-09-27 16:41:33 +0530

    math

---
+++ math.py
@@ -0,0 +1,38 @@
+# math functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+# abs()
+x = abs(-7.25)
+
+print(x)
+
+# pow(x, y)
+x = pow(4, 3)
+
+print(x)
+
+#import math
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+#math.ceil()
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+# pi
+import math
+
+x = math.pi
+
+print(x)
